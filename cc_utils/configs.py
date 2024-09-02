9d73b5b840fa82fc0ab70193751dd5e8e04712e3
Bryan Chan <chanb@beluga1.int.ets1.calculquebec.ca>
2024-09-02 10:47:20 -0400

    Add experiment

--- cc_utils/configs.py
+++ cc_utils/configs.py
@@ -23,6 +23,20 @@ EXPERIMENTS = {
             },
         ]
     },
+    "simple_icl-fixed_g-context_alpha": {
+        "run_time": "00:45:00",
+        "num_seeds": 10,
+        "variants": [
+            {
+                "key": "high_prob",
+                "values": [0.5, 0.67, 0.75, 0.8, 0.9, 0.99]
+            },
+            {
+                "key": "ground_truth_prob",
+                "values": [0.0, 0.1, 0.25, 0.5, 0.75, 0.9, 1.0]
+            },
+        ]
+    },
 }
 
 EVALUATION = {
