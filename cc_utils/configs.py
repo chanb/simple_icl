97973570945d1e4ba020887f6b1bedf01a0ca368
Bryan Chan <chanb@beluga1.int.ets1.calculquebec.ca>
2024-09-03 20:49:39 -0400

    Updated experiment configs

--- cc_utils/configs.py
+++ cc_utils/configs.py
@@ -9,6 +9,20 @@ EXPERIMENTS = {
             }
         ]
     },
+    "simple_icl-fixed_g-query_only_alpha": {
+        "run_time": "00:45:00",
+        "num_seeds": 10,
+        "variants": [
+            {
+                "key": "high_prob",
+                "values": [0.5, 0.67, 0.75, 0.8, 0.9, 0.99]
+            },
+            {
+                "key": "ground_truth_prob",
+                "values": [ii / 10 for ii in range(0, 11)]
+            },
+        ]
+    },
     "simple_icl-fixed_g": {
         "run_time": "00:45:00",
         "num_seeds": 10,
@@ -19,21 +33,7 @@ EXPERIMENTS = {
             },
             {
                 "key": "ground_truth_prob",
-                "values": [0.0, 0.1, 0.25, 0.5, 0.75, 0.9, 1.0]
-            },
-        ]
-    },
-    "simple_icl-fixed_g-context_alpha": {
-        "run_time": "00:45:00",
-        "num_seeds": 10,
-        "variants": [
-            {
-                "key": "high_prob",
-                "values": [0.5, 0.67, 0.75, 0.8, 0.9, 0.99]
-            },
-            {
-                "key": "ground_truth_prob",
-                "values": [0.0, 0.1, 0.25, 0.5, 0.75, 0.9, 1.0]
+                "values": [ii / 10 for ii in range(0, 11)]
             },
         ]
     },
