fa76de7196a43ab32c3ed6bf15df201b0b5d3e59
Bryan Chan <bryanchan13@gmail.com>
2024-09-07 17:49:49 -0600

    updated

--- cc_utils/configs.py
+++ cc_utils/configs.py
@@ -1,42 +1,42 @@
 EXPERIMENTS = {
-    "simple_icl-transformer": {
-        "run_time": "00:55:00",
-        "num_seeds": 10,
-        "variants": [
-            {
-                "key": "high_prob",
-                "values": [0.5, 0.67, 0.75, 0.8, 0.9, 0.99]
-            }
-        ]
-    },
-    "simple_icl-transformer-non_linear": {
-        "run_time": "00:55:00",
-        "num_seeds": 10,
-        "variants": [
-            {
-                "key": "high_prob",
-                "values": [0.5, 0.67, 0.75, 0.8, 0.9, 0.99]
-            },
-            {
-                "key": "num_high_prob_classes",
-                "values": [100],
-            },
-            {
-                "key": "num_low_prob_classes",
-                "values": [100],
-            }
-        ]
-    },
-    "simple_icl-learned_g": {
-        "run_time": "00:45:00",
-        "num_seeds": 10,
-        "variants": [
-            {
-                "key": "high_prob",
-                "values": [0.5, 0.67, 0.75, 0.8, 0.9, 0.99]
-            }
-        ]
-    },
+    # "simple_icl-transformer": {
+    #     "run_time": "00:55:00",
+    #     "num_seeds": 10,
+    #     "variants": [
+    #         {
+    #             "key": "high_prob",
+    #             "values": [0.5, 0.67, 0.75, 0.8, 0.9, 0.99]
+    #         }
+    #     ]
+    # },
+    # "simple_icl-transformer-non_linear": {
+    #     "run_time": "00:55:00",
+    #     "num_seeds": 10,
+    #     "variants": [
+    #         {
+    #             "key": "high_prob",
+    #             "values": [0.5, 0.67, 0.75, 0.8, 0.9, 0.99]
+    #         },
+    #         {
+    #             "key": "num_high_prob_classes",
+    #             "values": [100],
+    #         },
+    #         {
+    #             "key": "num_low_prob_classes",
+    #             "values": [100],
+    #         }
+    #     ]
+    # },
+    # "simple_icl-learned_g": {
+    #     "run_time": "00:45:00",
+    #     "num_seeds": 10,
+    #     "variants": [
+    #         {
+    #             "key": "high_prob",
+    #             "values": [0.5, 0.67, 0.75, 0.8, 0.9, 0.99]
+    #         }
+    #     ]
+    # },
     "simple_icl-fixed_g": {
         "run_time": "00:45:00",
         "num_seeds": 10,
