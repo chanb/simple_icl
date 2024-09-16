ffe82b33f58fa4ceb91ed7e1f125aa956352ac22
Bryan Chan <bryanchan13@gmail.com>
2024-09-16 17:36:09 -0600

    uncomment

--- local_utils/configs.py
+++ local_utils/configs.py
@@ -16,57 +16,57 @@ EXPERIMENTS = {
             },
         ]
     },
-    # "synthetic-iw_predictor": {
-    #     "num_seeds": 5,
-    #     "variants": [
-    #         {
-    #             "key": "p_high",
-    #             "values": [0.5, 0.75, 0.9, 0.99]
-    #         },
-    #         {
-    #             "key": "dataset_size",
-    #             "values": [2**6, 2**8, 2**10, 2**12, 2**14]
-    #         },
-    #         {
-    #             "key": "p_relevant_context",
-    #             "values": [0.0]
-    #         },
-    #     ]
-    # },
-    # "synthetic-ic_predictor": {
-    #     "num_seeds": 5,
-    #     "variants": [
-    #         {
-    #             "key": "p_high",
-    #             "values": [0.5, 0.75, 0.9, 0.99]
-    #         },
-    #         {
-    #             "key": "dataset_size",
-    #             "values": [2**6, 2**8, 2**10, 2**12, 2**14]
-    #         },
-    #         {
-    #             "key": "p_relevant_context",
-    #             "values": [0.5, 0.75, 0.9, 0.99]
-    #         },
-    #     ]
-    # },
-    # "synthetic-alpha": {
-    #     "num_seeds": 5,
-    #     "variants": [
-    #         {
-    #             "key": "p_high",
-    #             "values": [0.5, 0.75, 0.9, 0.99]
-    #         },
-    #         {
-    #             "key": "dataset_size",
-    #             "values": [2**6, 2**8, 2**10, 2**12, 2**14]
-    #         },
-    #         {
-    #             "key": "p_relevant_context",
-    #             "values": [0.5, 0.75, 0.9, 0.99]
-    #         },
-    #     ]
-    # },
+    "synthetic-iw_predictor": {
+        "num_seeds": 5,
+        "variants": [
+            {
+                "key": "p_high",
+                "values": [0.5, 0.75, 0.9, 0.99]
+            },
+            {
+                "key": "dataset_size",
+                "values": [2**6, 2**8, 2**10, 2**12, 2**14]
+            },
+            {
+                "key": "p_relevant_context",
+                "values": [0.0]
+            },
+        ]
+    },
+    "synthetic-ic_predictor": {
+        "num_seeds": 5,
+        "variants": [
+            {
+                "key": "p_high",
+                "values": [0.5, 0.75, 0.9, 0.99]
+            },
+            {
+                "key": "dataset_size",
+                "values": [2**6, 2**8, 2**10, 2**12, 2**14]
+            },
+            {
+                "key": "p_relevant_context",
+                "values": [0.5, 0.75, 0.9, 0.99]
+            },
+        ]
+    },
+    "synthetic-alpha": {
+        "num_seeds": 5,
+        "variants": [
+            {
+                "key": "p_high",
+                "values": [0.5, 0.75, 0.9, 0.99]
+            },
+            {
+                "key": "dataset_size",
+                "values": [2**6, 2**8, 2**10, 2**12, 2**14]
+            },
+            {
+                "key": "p_relevant_context",
+                "values": [0.5, 0.75, 0.9, 0.99]
+            },
+        ]
+    },
 }
 
 EVALUATION = {
