98ea0886e04260b10d178a2023c4623febf59007
Bryan Chan <bryanchan13@gmail.com>
2024-09-17 23:16:23 -0600

    fix

--- cc_utils/configs.py
+++ cc_utils/configs.py
@@ -48,28 +48,6 @@ EXPERIMENTS = {
     #         },
     #     ]
     # },
-    "synthetic-transformer-num_relevant_contexts-noisy_inputs_0.2": {
-        "run_time": "00:45:00",
-        "num_seeds": 5,
-        "variants": [
-            {
-                "key": "p_high",
-                "values": [0.9]
-            },
-            {
-                "key": "dataset_size",
-                "values": [2**6, 2**8, 2**10, 2**12, 2**14]
-            },
-            {
-                "key": "num_relevant_contexts",
-                "values": [1, 2, 3, 4]
-            },
-            {
-                "key": "p_relevant_context",
-                "values": [0.0, 0.9, 1.0]
-            },
-        ]
-    },
     # "synthetic-transformer-no_noise": {
     #     "run_time": "00:30:00",
     #     "num_seeds": 5,
@@ -160,6 +138,24 @@ EXPERIMENTS = {
     #         },
     #     ]
     # },
+    "synthetic-transformer-large_num_low_freq-input_noise_0.2": {
+        "run_time": "00:55:00",
+        "num_seeds": 5,
+        "variants": [
+            {
+                "key": "p_high",
+                "values": [0.9]
+            },
+            {
+                "key": "dataset_size",
+                "values": [2**8, 2**10, 2**12, 2**14]
+            },
+            {
+                "key": "p_relevant_context",
+                "values": [0.0, 0.9, 1.0]
+            },
+        ]
+    },
 }
 
 EVALUATION = {
