e9232ff8d06e8893860334d37031ade05bb4175f
Bryan Chan <bryanchan13@gmail.com>
2024-09-17 23:13:33 -0600

    extra exp

--- cc_utils/configs.py
+++ cc_utils/configs.py
@@ -48,6 +48,28 @@ EXPERIMENTS = {
             },
         ]
     },
+    "synthetic-transformer-num_relevant_contexts-noisy_inputs_0.2": {
+        "run_time": "00:45:00",
+        "num_seeds": 5,
+        "variants": [
+            {
+                "key": "p_high",
+                "values": [0.9]
+            },
+            {
+                "key": "dataset_size",
+                "values": [2**6, 2**8, 2**10, 2**12, 2**14]
+            },
+            {
+                "key": "num_relevant_contexts",
+                "values": [1, 2, 3, 4]
+            },
+            {
+                "key": "p_relevant_context",
+                "values": [0.0, 0.9, 1.0]
+            },
+        ]
+    },
     "synthetic-transformer-no_noise": {
         "run_time": "00:30:00",
         "num_seeds": 5,
