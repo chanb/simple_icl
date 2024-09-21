f1031f4e2b690a43a299452821d5f314af39c686
Bryan Chan <bryanchan13@gmail.com>
2024-09-21 14:32:42 -0600

    update

--- cc_utils/configs.py
+++ cc_utils/configs.py
@@ -1,6 +1,5 @@
 # TODO: Omniglot
 # TODO: Simple ICL model
-# TODO: More thorough experiments
 
 # Section 5: Run for longer with added experiments
 EXPERIMENTS = {
@@ -10,9 +9,9 @@ EXPERIMENTS = {
         "num_seeds": 5,
         "variants": [
             {
+                # NOTE: Need to rerun all because increasing number of gradient steps to 100k
                 "key": "dataset_size",
-                # "values": [2**6, 2**8, 2**10, 2**12, 2**14, 2**16, 2**18]
-                "values": [2**20]
+                "values": [2**6, 2**8, 2**10, 2**12, 2**14, 2**16, 2**18, 2**20]
             },
             {
                 "key": "p_relevant_context",
@@ -76,24 +75,24 @@ EXPERIMENTS = {
         ]
     },
     # Section 5.1.4
-    # "synthetic-transformer-num_low_freq": {
-    #     "run_time": "10:00:00",
-    #     "num_seeds": 5,
-    #     "variants": [
-    #         {
-    #             "key": "dataset_size",
-    #             "values": [2**10, 2**12, 2**14, 2**16, 2**18, 2**20]
-    #         },
-    #         {
-    #             "key": "p_relevant_context",
-    #             "values": [0.0, 0.9, 1.0]
-    #         },
-    #         {
-    #             "key": "num_low_prob_classes",
-    #             "values": [5, 45, 95, 495]
-    #         }
-    #     ]
-    # },
+    "synthetic-transformer-num_low_freq": {
+        "run_time": "10:00:00",
+        "num_seeds": 5,
+        "variants": [
+            {
+                "key": "dataset_size",
+                "values": [2**10, 2**12, 2**14, 2**16, 2**18, 2**20]
+            },
+            {
+                "key": "p_relevant_context",
+                "values": [0.0, 0.9, 1.0]
+            },
+            {
+                "key": "num_low_prob_classes",
+                "values": [5, 45, 95, 495]
+            }
+        ]
+    },
 }
 
 # Section 5
@@ -223,164 +222,6 @@ EXPERIMENTS = {
                 "key": "num_low_prob_classes",
                 "values": [5, 45, 95, 495]
             }
-        ]
-    },
-}
-"""
-
-"""
-# Preliminarty experiments on synthetic dataset
-EXPERIMENTS = {
-    "synthetic-transformer-context_len": {
-        "run_time": "00:45:00",
-        "num_seeds": 5,
-        "variants": [
-            {
-                "key": "p_high",
-                "values": [0.9]
-            },
-            {
-                "key": "dataset_size",
-                "values": [2**6, 2**8, 2**10, 2**12, 2**14]
-            },
-            {
-                "key": "num_contexts",
-                "values": [2, 4, 8]
-            },
-            {
-                "key": "p_relevant_context",
-                "values": [0.0, 0.9, 1.0]
-            },
-        ]
-    },
-    "synthetic-transformer-num_relevant_contexts": {
-        "run_time": "00:45:00",
-        "num_seeds": 5,
-        "variants": [
-            {
-                "key": "p_high",
-                "values": [0.9]
-            },
-            {
-                "key": "dataset_size",
-                "values": [2**6, 2**8, 2**10, 2**12, 2**14]
-            },
-            {
-                "key": "num_relevant_contexts",
-                "values": [1, 2, 3, 4]
-            },
-            {
-                "key": "p_relevant_context",
-                "values": [0.0, 0.9, 1.0]
-            },
-        ]
-    },
-    "synthetic-transformer-no_noise": {
-        "run_time": "00:30:00",
-        "num_seeds": 5,
-        "variants": [
-            {
-                "key": "p_high",
-                "values": [0.9]
-            },
-            {
-                "key": "dataset_size",
-                "values": [2**6, 2**8, 2**10, 2**12, 2**14]
-            },
-            {
-                "key": "p_relevant_context",
-                "values": [0.0, 0.9, 1.0]
-            },
-        ]
-    },
-    "synthetic-transformer-noisy_inputs_0.2": {
-        "run_time": "00:30:00",
-        "num_seeds": 5,
-        "variants": [
-            {
-                "key": "p_high",
-                "values": [0.9]
-            },
-            {
-                "key": "dataset_size",
-                "values": [2**6, 2**8, 2**10, 2**12, 2**14]
-            },
-            {
-                "key": "p_relevant_context",
-                "values": [0.0, 0.9, 1.0]
-            },
-        ]
-    },
-    "synthetic-transformer-noisy_labels_0.1": {
-        "run_time": "00:30:00",
-        "num_seeds": 5,
-        "variants": [
-            {
-                "key": "p_high",
-                "values": [0.9]
-            },
-            {
-                "key": "dataset_size",
-                "values": [2**6, 2**8, 2**10, 2**12, 2**14]
-            },
-            {
-                "key": "p_relevant_context",
-                "values": [0.0, 0.9, 1.0]
-            },
-        ]
-    },
-    "synthetic-transformer-noisy_labels_0.01": {
-        "run_time": "00:30:00",
-        "num_seeds": 5,
-        "variants": [
-            {
-                "key": "p_high",
-                "values": [0.9]
-            },
-            {
-                "key": "dataset_size",
-                "values": [2**6, 2**8, 2**10, 2**12, 2**14]
-            },
-            {
-                "key": "p_relevant_context",
-                "values": [0.0, 0.9, 1.0]
-            },
-        ]
-    },
-    "synthetic-transformer-large_num_low_freq": {
-        "run_time": "00:55:00",
-        "num_seeds": 5,
-        "variants": [
-            {
-                "key": "p_high",
-                "values": [0.9]
-            },
-            {
-                "key": "dataset_size",
-                "values": [2**8, 2**10, 2**12, 2**14]
-            },
-            {
-                "key": "p_relevant_context",
-                "values": [0.0, 0.9, 1.0]
-            },
-        ]
-    },
-    "synthetic-transformer-large_num_low_freq-input_noise_0.2": {
-        "run_time": "00:55:00",
-        "num_seeds": 5,
-        "variants": [
-            {
-                "key": "p_high",
-                "values": [0.9]
-            },
-            {
-                "key": "dataset_size",
-                "values": [2**8, 2**10, 2**12, 2**14]
-            },
-            {
-                "key": "p_relevant_context",
-                "values": [0.0, 0.9, 1.0]
-            },
         ]
     },
 }
