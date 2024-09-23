1029318f451396d54f5e5143a65c29505429c481
Bryan Chan <bryan.chan@ualberta.ca>
2024-09-22 20:25:05 -0600

    update

--- local_utils/configs.py
+++ local_utils/configs.py
@@ -1,7 +1,32 @@
+# EXPERIMENTS = {
+#     "omniglot-input_noise": {
+#         "num_seeds": 3,
+#         "variants": [
+#             {
+#                 "key": "dataset_size",
+#                 "values": [10000, 100000, 1000000]
+#             },
+#             {
+#                 "key": "p_relevant_context",
+#                 "values": [0.0, 0.9, 1.0]
+#             },
+#             {
+#                 "key": "input_noise_std",
+#                 "values": [0.0, 0.1, 0.2]
+#             },
+#         ]
+#     },
+# }
+
+# Preliminary
 EXPERIMENTS = {
     "omniglot-input_noise": {
-        "num_seeds": 3,
+        "num_seeds": 1,
         "variants": [
+            {
+                "key": "dataset_size",
+                "values": [10000, 100000, 1000000]
+            },
             {
                 "key": "p_relevant_context",
                 "values": [0.0, 0.9, 1.0]
