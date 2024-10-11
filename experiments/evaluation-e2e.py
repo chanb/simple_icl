f67f1b6dc6c70e130256160efadb48005d9149a7
Bryan Chan <chanb@beluga3.int.ets1.calculquebec.ca>
2024-10-10 23:36:03 -0400

    udpate

--- experiments/evaluation-e2e.py
+++ experiments/evaluation-e2e.py
@@ -268,15 +268,22 @@ def main(args: SimpleNamespace):
 
             if model_type == "alpha":
                 curr_model = model
+                curr_params = params
             elif model_type == "iw":
                 curr_model = model.iw_predictor
+                curr_params = {
+                    CONST_MODEL: params[CONST_MODEL]["iw_predictor"]
+                }
             elif model_type == "ic":
                 curr_model = model.ic_predictor
+                curr_params = {
+                    CONST_MODEL: params[CONST_MODEL]["ic_predictor"]
+                }
 
             for eval_name in prefetched_data:
                 aux = evaluate(
                     model=curr_model,
-                    params=params,
+                    params=curr_params,
                     prefetched_data=prefetched_data[eval_name],
                     max_label=None,
                     sample_key=sample_key,
